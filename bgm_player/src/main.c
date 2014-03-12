4510f0e461a56105efeb0c492b95a708357125d6
yogpstop <git@yogpc.com>
2014-03-12 14:21:32 +0900

    Fix something  - Permissions  - use scheduling policy now  - More useful building file

--- bgm_player/src/main.c
+++ bgm_player/src/main.c
@@ -8,8 +8,15 @@ int main(int argc, char **argv) {
 	listing(argv+1);
 	list_shuffle();
 	pthread_t play, buffer;
+	pthread_attr_t rt;
+	struct sched_param p = {};
+	p.sched_priority = sched_get_priority_max(SCHED_RR);
+	pthread_attr_init(&rt);
+	pthread_attr_setinheritsched(&rt, PTHREAD_EXPLICIT_SCHED);
+	pthread_attr_setschedpolicy(&rt, SCHED_RR);
+	pthread_attr_setschedparam(&rt, &p);
 	pthread_create(&buffer, NULL, &buffer_thread, NULL);
-	pthread_create(&play, NULL, &play_thread, NULL);
+	pthread_create(&play, &rt, &play_thread, NULL);
 	console();
 	pthread_join(buffer, NULL);
 	pthread_join(play, NULL);
