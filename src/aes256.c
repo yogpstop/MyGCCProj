3ede055d3a85fb97cd24a7eaf39d87576a985bac
yogpstop <github@yogpc.com>
2014-05-23 15:44:44 +0900

    Fix memory leak and get more secure

--- src/aes256.c
+++ src/aes256.c
@@ -14,41 +14,40 @@
 #include <openssl/evp.h>
 #include <openssl/aes.h>
 #include <openssl/err.h>
-int aes256(const unsigned char* raw, int len, const unsigned char key[256/8],
-	const unsigned char iv[128/8], unsigned char** enc) {
+int aes256(const unsigned char* in, int ilen, const unsigned char key[256/8],
+	const unsigned char iv[128/8], unsigned char** out, int mode) {
+	int mlen = ilen + AES_BLOCK_SIZE - (ilen % AES_BLOCK_SIZE),
+		olen = mlen, flen = 0;
+	*out = malloc(mlen);
+	if(out == NULL)
+		return 0;
+	memset(*out, 0, mlen);
 	EVP_CIPHER_CTX evpctx;
-	int elen = len + AES_BLOCK_SIZE, flen = 0;
 	EVP_CIPHER_CTX_init(&evpctx);
-	EVP_EncryptInit_ex(&evpctx, EVP_aes_256_cbc(), NULL, key, iv);
-	*enc = malloc(elen);
-	memset(*enc, 0, elen);
-	EVP_EncryptInit_ex(&evpctx, NULL, NULL, NULL, NULL);
-	EVP_EncryptUpdate(&evpctx, *enc, &elen, raw, len);
-	EVP_EncryptFinal_ex(&evpctx, *enc + elen, &flen);
-	return elen + flen;
-}
-int aes256d(const unsigned char* raw, int len, const unsigned char key[256/8],
-	const unsigned char iv[128/8], unsigned char** dec) {
-	EVP_CIPHER_CTX evpctx;
-	int dlen = len, flen = 0;
-	EVP_CIPHER_CTX_init(&evpctx);
-	EVP_DecryptInit_ex(&evpctx, EVP_aes_256_cbc(), NULL, key, iv);
-	*dec = malloc(dlen+AES_BLOCK_SIZE);
-	memset(*dec, 0, dlen+AES_BLOCK_SIZE);
-	EVP_DecryptInit_ex(&evpctx, NULL, NULL, NULL, NULL);
-	EVP_DecryptUpdate(&evpctx, *dec, &dlen, raw, len);
-	EVP_DecryptFinal_ex(&evpctx, *dec + dlen, &flen);
-	return dlen + flen;
+	if(1 != EVP_CipherInit_ex(&evpctx, EVP_aes_256_cbc(), NULL, key, iv, mode))
+		goto error;
+	if(1 != EVP_CipherInit_ex(&evpctx, NULL, NULL, NULL, NULL, mode))
+		goto error;
+	if(1 != EVP_CipherUpdate(&evpctx, *out, &olen, in, ilen))
+		goto error;
+	if(1 != EVP_CipherFinal_ex(&evpctx, *out + olen, &flen))
+		goto error;
+	EVP_CIPHER_CTX_cleanup(&evpctx);
+	return olen + flen;
+error:
+	EVP_CIPHER_CTX_cleanup(&evpctx);
+	return -mlen;
 }
 #ifdef _WIN32
 static HANDLE conin;
 #endif
 size_t input(const char* msg, unsigned char **buf) {
 	unsigned char *cur, *end;
-	size_t s = 8;
+	size_t s = AES_BLOCK_SIZE;
 	*buf = cur = malloc(s);
+	if(!cur) return 0;
 	memset(*buf, 0, s);
-	end = *buf + s;
+	end = *buf + s - 1;
 #ifndef _WIN32
 	__fpurge(stdin);
 #else
@@ -57,12 +56,23 @@ size_t input(const char* msg, unsigned char **buf) {
 	fputs(msg, stdout);
 	do {
 		fread(cur, 1, 1, stdin);
-		if(end < cur) {
-			s <<= 1;
-			end = realloc(*buf, s);
+		if(end <= cur) {
+			s += AES_BLOCK_SIZE;
+			end = malloc(s);
+			if(!end) {
+				memset(*buf, 0, s - AES_BLOCK_SIZE);
+				free(*buf);
+				*buf = NULL;
+				fputc('\n', stdout);
+				return 0;
+			}
+			memset(end, 0, s);
+			memcpy(end, *buf, s - AES_BLOCK_SIZE);
+			memset(*buf, 0, s - AES_BLOCK_SIZE);
 			cur += end - *buf;
+			free(*buf);
 			*buf = end;
-			end = *buf + s;
+			end = *buf + s - 1;
 		}
 	} while(*cur++ != 0x04);
 	cur--;
@@ -70,25 +80,26 @@ size_t input(const char* msg, unsigned char **buf) {
 	return cur - *buf;
 }
 int main(int argc, char* argv[]) {
+	FILE *f = NULL;
+	unsigned char *raw = NULL, *rpass = NULL, *riv = NULL, *out = NULL,
+		pass[256 / 8], iv[128 / 8];
+	size_t lraw = 0, lrpass = 0, lriv = 0;
+	int lout = 0, enc = 0;
 	if(argc!=3) {
 		fputs("Usage: <executable> [e/d] [encrypted file name]\n", stderr);
 		return 1;
 	}
-	int enc;
 	if((argv[1][0] & 0x5F) == 'D') enc = 0;
 	else if((argv[1][0] & 0x5F) == 'E') enc = 1;
 	else {
 		fputs("Usage: <executable> [e/d] [encrypted file name]\n", stderr);
 		return 2;
 	}
-	FILE *f;
 	f = fopen(argv[2], enc ? "wb" : "rb");
 	if(!f) {
 		fputs("Usage: <executable> [e/d] [encrypted file name]\n", stderr);
 		return 3;
 	}
-	unsigned char *raw, *rpass, *riv, pass[256 / 8], iv[128 / 8], *out;
-	size_t lraw, lrpass, lriv, lout;
 #ifndef _WIN32
 	struct termios str, bak;
 	tcgetattr(STDIN_FILENO, &str);
@@ -108,10 +119,13 @@ int main(int argc, char* argv[]) {
 			"    your encrypted data is dangerous.\n", stdout);
 	fputs("WARNING: In this console,\n    you must type Ctrl-D key\n"
 		"    when you complete type password.\n", stdout);
-	if(enc)
+	if(enc) {
 		lraw = input("Please type password to encrypted (no echo):",&raw);
+		if (!raw) goto myfree;
+	}
 	lrpass = input("Please type first password (no echo):",&rpass);
 	lriv = input("Please type second password (no echo):",&riv);
+	if (!rpass || !riv) goto myfree;
 #ifndef _WIN32
 	tcsetattr(STDIN_FILENO, TCSANOW, &bak);
 #else
@@ -120,18 +134,41 @@ int main(int argc, char* argv[]) {
 	SHA256(rpass, lrpass, pass);
 	MD5(riv, lriv, iv);
 	if(enc) {
-		lout = aes256(raw, lraw, pass, iv, &out);
+		lout = aes256(raw, lraw, pass, iv, &out, 1);
+		if (lout <= 0) goto myfree;
 		fwrite(out, 1, lout, f);
 	} else {
 		fseek(f, 0, SEEK_END);
 		lraw = ftell(f);
 		raw = malloc(lraw);
+		if (!raw) goto myfree;
 		fseek(f, 0, SEEK_SET);
 		fread(raw, 1, lraw, f);
-		lout = aes256d(raw, lraw, pass, iv, &out);
+		lout = aes256(raw, lraw, pass, iv, &out, 0);
+		if (lout <= 0) goto myfree;
 		fputs("Your password is\n", stdout);
 		fwrite(out, 1, lout, stdout);
 		fputs("\n", stdout);
+	}
+myfree:
+	memset(iv, 0, 128 / 8);
+	memset(pass, 0, 256 / 8);
+	if(riv) {
+		memset(riv, 0, lriv);
+		free(riv);
+	}
+	if(rpass) {
+		memset(rpass, 0, lrpass);
+		free(rpass);
+	}
+	if(raw) {
+		memset(raw, 0, lraw);
+		free(raw);
+	}
+	if(out) {
+		if(lout < 0) lout = -lout;
+		memset(out, 0, lout);
+		free(out);
 	}
 	fclose(f);
 	return 0;
